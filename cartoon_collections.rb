e06a6a77b6aed3d38bf8a6873aafb43177e5b216
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-22 17:29:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array_strings)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array_strings.each do |stuff|
+    
+  
 end
